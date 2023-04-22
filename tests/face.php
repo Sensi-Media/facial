ca4ed55be092361e67a140365aa04e022e4efe7a
Marijn Ophorst <marijn@sensimedia.nl>
2023-04-22 16:23:14 +0200

    update tests to new interface

--- tests/face.php
+++ tests/face.php
@@ -5,15 +5,15 @@ return function () : Generator {
     /** An image with a face gets detected */
     yield function () {
         $detector = new Sensi\Facial\Detector;
-        $result = $detector->fromFile(dirname(__DIR__).'/resources/face.jpg')->detectFace();
-        assert($result === true);
+        $result = $detector->fromFile(dirname(__DIR__).'/resources/face.jpg');
+        assert($result->hasFace() === true);
     };
 
     /** An image without a face returns false */
     yield function () {
         $detector = new Sensi\Facial\Detector;
-        $result = $detector->fromFile(dirname(__DIR__).'/resources/noface.jpg')->detectFace();
-        assert($result === false);
+        $result = $detector->fromFile(dirname(__DIR__).'/resources/noface.jpg');
+        assert($result->hasFace() === false);
     };
 };
 
